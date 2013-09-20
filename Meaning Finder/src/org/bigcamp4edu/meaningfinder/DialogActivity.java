9cdf737d4c80a310bcd1a623382e6fe54513ae31
taycleedtest <taycleed@naver.com>
2013-09-20 15:33:05 +0900

    Lock된 기기를 깨우면서 Notify 함.

--- Meaning Finder/src/org/bigcamp4edu/meaningfinder/DialogActivity.java
+++ Meaning Finder/src/org/bigcamp4edu/meaningfinder/DialogActivity.java
@@ -2,15 +2,22 @@ package org.bigcamp4edu.meaningfinder;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Application;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.view.WindowManager;
 
 public class DialogActivity extends Activity {
 	
 	protected void onCreate(android.os.Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+                | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+
 		
 		new AlertDialog.Builder(this)
 	    	.setIcon(R.drawable.icon_launcher)
@@ -30,6 +37,12 @@ public class DialogActivity extends Activity {
 	        })
 	        .setNegativeButton(R.string.noti_no, new DialogInterface.OnClickListener() {
 	            public void onClick(DialogInterface dialog, int id) {
+	            	moveTaskToBack(true);
+//	            	Intent intent = new Intent(Intent.ACTION_MAIN);
+//	            	intent.addCategory(Intent.CATEGORY_HOME);
+//	            	intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//	            	startActivity(intent);
+
 	            	finish();
 	            }
 	        })
