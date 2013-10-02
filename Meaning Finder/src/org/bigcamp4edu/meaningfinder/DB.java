e4f1f55b111ea88f5537ee7c2b25d43bb97aa587
taycleed <taycleed@gmail.com>
2013-10-02 14:56:28 +0900

    API 업데이트 및 회원가입 폼의 생일 입력 버그 수정.

--- Meaning Finder/src/org/bigcamp4edu/meaningfinder/DB.java
+++ Meaning Finder/src/org/bigcamp4edu/meaningfinder/DB.java
@@ -23,7 +23,7 @@ public class DB {
 	// 앱 공통 주소
 	public final static String loginChkUrl		= "http://mean1.cloudapp.net/api/api_userLogin.php";						// 로그인 체크
 	public final static String joinChkUrl		= "http://mean1.cloudapp.net/api/api_userJoinProc.php";						// 회원가입
-	public final static String listUrl			= "http://mean1.cloudapp.net/api/_api_requestionList.php";					// 질문 리스트
+	public final static String listUrl			= "http://mean1.cloudapp.net/api/api_requestionList.php";					// 질문 리스트
 	public final static String answerINfoUrl	= "http://mean1.cloudapp.net/api/api_requestionInfo.php";					// 답변내용 상세페이지
 	public final static String getQuestionUrl	= "http://mean1.cloudapp.net/api/api_getQuestion.php";						// 질문 얻어오기
 	public final static String answerJoinUrl	= "http://mean1.cloudapp.net/api/api_insert_answer.php";					// 답변 작성하기
